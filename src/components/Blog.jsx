51bc5d9ac05dab944e425e90fc103d33152e8f8b
Mike Schutte <tmikeschutte@gmail.com>
2017-03-02 16:21:35 -0700

    Remove unnecessary containers

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -4,14 +4,12 @@ import '../stylesheets/App.css';
 export default class Blog extends Component {
   render() {
     return (
-      <div className="container">
-        <article className="blog">
-          <a href="https://medium.com/@tmikeschutte" target="_blank"><img src="https://assets.ifttt.com/images/channels/1259075256/icons/on_color_large.png" alt="medium logo"/></a>
-          <span>←</span>
-          <span>↑</span>
-          <p> Find me on Medium.</p>
-        </article>
-      </div>
+      <article className="blog">
+        <a href="https://medium.com/@tmikeschutte" target="_blank"><img src="https://assets.ifttt.com/images/channels/1259075256/icons/on_color_large.png" alt="medium logo"/></a>
+        <span>←</span>
+        <span>↑</span>
+        <p> Find me on Medium.</p>
+      </article>
     );
   }
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,14 +4,12 @@ import '../stylesheets/App.css';
 export default class Contact extends Component {
   render() {
     return (
-      <div className="container">
-        <article className="contact">
-          <a href="https://github.com/tmikeschu" target="_blank"><img src="https://image.flaticon.com/icons/svg/25/25231.svg" alt="github"/></a>
-          <a href="https://www.linkedin.com/in/tmikeschu" target="_blank"><img src="https://travellingmysteryguest.files.wordpress.com/2014/03/linkedin.png" alt="linkedin"/></a>
-          <a href="mailto:tmikeschutte@gmail.com" target="_blank"><img src="http://www.clker.com/cliparts/5/S/U/Y/A/R/email-icon-hi.png" alt="github"/></a>
-          <a href="https://twitter.com/tmikeschu" target="_blank"><img src="http://icons.iconarchive.com/icons/uiconstock/socialmedia/512/Twitter-icon.png" alt="twitter"/></a>
-        </article>
-      </div>
+      <article className="contact">
+        <a href="https://github.com/tmikeschu" target="_blank"><img src="https://image.flaticon.com/icons/svg/25/25231.svg" alt="github"/></a>
+        <a href="https://www.linkedin.com/in/tmikeschu" target="_blank"><img src="https://travellingmysteryguest.files.wordpress.com/2014/03/linkedin.png" alt="linkedin"/></a>
+        <a href="mailto:tmikeschutte@gmail.com" target="_blank"><img src="http://www.clker.com/cliparts/5/S/U/Y/A/R/email-icon-hi.png" alt="github"/></a>
+        <a href="https://twitter.com/tmikeschu" target="_blank"><img src="http://icons.iconarchive.com/icons/uiconstock/socialmedia/512/Twitter-icon.png" alt="twitter"/></a>
+      </article>
     );
   }
 }
