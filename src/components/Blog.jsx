bde55fdc2ae174ff5ad086890611119f15b68871
Mike Schutte <tmikeschutte@gmail.com>
2017-07-19 13:01:11 -0600

    Update blog link

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Blog = () => (
   <article className="blog">
-    <a href="https://medium.com/@tmikeschutte" target="_blank" rel="noopener noreferrer"><img src="https://assets.ifttt.com/images/channels/1259075256/icons/on_color_large.png" alt="medium logo"/></a>
+    <a href="https://medium.com/@tmikeschu" target="_blank" rel="noopener noreferrer"><img src="https://assets.ifttt.com/images/channels/1259075256/icons/on_color_large.png" alt="medium logo"/></a>
     <span>←</span>
     <span>↑</span>
     <p> Find me on Medium.</p>
